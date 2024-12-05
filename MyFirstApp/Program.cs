177e16f46d6a05abea3ff2de37138e23ebb13625
Joosep Angast <joosep.angastrlive.com>
2024-12-05 13:06:26 +0200

    My First App

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// This is a comment to my code
+
+// Rakendus küsib kasutajal küsida tema nime
+// Rakendus tervitab kasutajat nimepidi
+Console.WriteLine("Enter your name:"); //Output
+// string - sõne
+string userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+// string interpolation
+
+Console.WriteLine($"Hello, {userName}!");
